d8623b8c6f14cdec2c9503b41053fd81174c5a08
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-03-28 11:31:35 -0300

    Initial commit

---
+++ Assets/scripts/Ejercicio_2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+////Realizá un programa que, siendo num1 y num2 variables enteras con valores 4 y 5,  respectivamente, realice la operación correspondiente y muestre el resultado en pantalla: 
+//a. Suma 
+//b. Producto 
+
+public class Ejercicio_2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 4;
+        int num2 = 5;
+        Debug.Log("El resultado de la suma es " + (num1 + num2));
+        Debug.Log("El resultado de la multiplicacion es " + num1 * num2);
+    }
+
+}
---
+++ Assets/scripts/Ejercicio_4.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+////Realizá un programa que permita ingresar dos números enteros num1 y num2 por Inspector.
+//Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena utilizando el siguiente formato:
+//“La  suma entre “ + num1 + “ y “ + num2 + “ da como resultado “ + num1 + num2. 
+
+public class Ejercicio_4 : MonoBehaviour
+   
+{
+    // Start is called before the first frame update
+    public int num1,num2;
+    void Start()
+    {
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + (num1 + num2));
+    }
+
+}
