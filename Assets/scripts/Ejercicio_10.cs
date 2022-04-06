9b5c64b358e393145c8cc1cf2d0e5901782081a8
YannickNepo <101335474+YannickNepo@users.noreply.github.com>
2022-04-06 11:34:52 -0300

    ejercicio 9 y 10

---
+++ Assets/scripts/Ejercicio_10.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Realizá un programa que resuelva el siguiente problema:
+//Nos piden desarrollar un programa que informe si un estacionamiento se encuentra o no abierto según la hora ingresada por el usuario.
+//Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs.
+//(Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).
+//Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el
+//Inspector, y deberemos setear una variable booleana llamada estaAbierto en true o false,
+//según corresponda, e informar por pantalla mediante la misma si el estacionamiento se
+//encuentra abierto o no.Obs: En el caso de que el valor de la variable correspona a una hora
+//menor a 0 o mayor a 24, mostrar el mensaje: “Ha ingresado una hora incorrecta”
+
+public class Ejercicio_10 : MonoBehaviour
+{
+    public int hora;
+    public bool estaAbierto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (hora >= 10 && hora <= 18) 
+        {
+            estaAbierto = true;
+            Debug.Log("El estacionamiento esta abierto");
+        }
+        if (hora > 0 && hora < 10)
+        {
+            estaAbierto = false;
+            Debug.Log("El estacionamiento esta cerrado");
+        }
+        if (hora > 18 && hora < 24)
+        {
+            estaAbierto = false;
+            Debug.Log("El estacionamiento esta cerrado");
+        }
+        if (hora < 0 || hora > 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta");
+        }
+    }
+
+    
+}
--- Assets/scripts/Ejercicio_9.cs
+++ Assets/scripts/Ejercicio_9.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Realizá un programa que resuelva el siguiente problema:  
+//Tres personas aportan diferente capital a una sociedad.
+//Se desea saber qué porcentaje del  total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado  por las tres. 
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego  mostrar lo pedido en el siguiente formato:
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ …. 
+
+
 public class Ejercicio_9 : MonoBehaviour
 {
+    public string nombre1;
+    public string nombre2;
+    public string nombre3;
+
+    public float capital1;
+    public float capital2;
+    public float capital3;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float capitaltotal = capital1 + capital2 + capital3;
+        float porcentaje1 = capital1 * 100 / capitaltotal;
+        float porcentaje2 = capital2 * 100 / capitaltotal;
+        float porcentaje3 = capital3 * 100 / capitaltotal;
+        Debug.Log("El total aportado es de" + capitaltotal + "pesos");
+        Debug.Log(nombre1 + " Aporto" + capital1 + "pesos." + "El porcentaje de la capital es de" + porcentaje1 + "% del capital");
+        Debug.Log(nombre2 + " Aporto" + capital2 + "pesos." + "El porcentaje de la capital es de" + porcentaje2  + "% del capital");
+        Debug.Log(nombre3 + " Aporto" + capital3 + "pesos." + "El porcentaje de la capital es de" + porcentaje3 + "% del capital");
     }
 }
